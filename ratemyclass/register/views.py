753258ef7b097fb5313f347df835543da420b389
brstrozy <brstrozy@cheshire.cse.buffalo.edu>
2022-03-10 23:45:10 -0500

    #13 login page now displays correctly on django server

--- ratemyclass/register/views.py
+++ ratemyclass/register/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def login(response):
-    return HttpResponse("<h1>This is the Login Page</h1>")
+    return render(response, "register/login.html", {})
+    # return HttpResponse("<h1>This is the Login Page</h1>")
 
 def register(response):
-    return HttpResponse("<h1>This is the Register Page</h1>")
+    return render(response, "register/register.html", {})
