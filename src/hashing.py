6ab647b1605fd6c149893774e5ef4af2171dee2a
jadonner <jadonner@buffalo.edu>
2022-03-10 19:09:37 -0500

    #32 Reworked the function into a hashing and decrypting password

--- src/hashing.py
+++ src/hashing.py
@@ -4,9 +4,11 @@ def password_hash(password):
     # Hash a password for the first time, with a randomly-generated salt
     password = password.encode()
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed
     # Check that an unhashed password matches one that has previously been hashed
+
+def check_password(password, hashed):
     if bcrypt.checkpw(password, hashed):
         print("It matches!")
-        return hashed
     else:
         print("It does not match :(")
