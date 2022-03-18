05c74ce627468a2bedc089131b53fd84389a0220
Nicholas M Tryon <nmtryon@cheshire.cse.buffalo.edu>
2022-03-17 23:39:52 -0400

    #11 added registration backend

--- ratemyclass/register/views.py
+++ ratemyclass/register/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from .forms import CreateNewList
+from .forms import RegisterForm
+
 # Create your views here.
 def login(response):
     return render(response, "register/login.html", {})
     # return HttpResponse("<h1>This is the Login Page</h1>")
 
 def register(response):
-    return render(response, "register/register.html", {})
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/home")
+    else:
+        form = RegisterForm()
+    return render(response, "register/register.html", {"form":form})
