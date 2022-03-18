02ea60c119b56dca1a5942261824f14dd58165a3
Joshua A Donner <jadonner@cheshire.cse.buffalo.edu>
2022-03-18 13:24:00 -0400

    Password reset functionality works, Login button at the end of the process does not redirect to the correct page

--- ratemyclass/main/urls.py
+++ ratemyclass/main/urls.py
@@ -12,21 +12,21 @@ urlpatterns = [
     path("password-reset/", ResetPasswordView.as_view(), name="password_reset"),
 
 
-    path('reset_password/',
-     auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
-     name="reset_password"),
+    #path('reset_password/',
+     #auth_views.PasswordResetView.as_view(template_name="cse442-spring2022-team-the-howdies/ratemyclass/main/templates/password_reset.html"),
+     #name="reset_password"),
 
-    path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), 
-        name="password_reset_done"),
+    #path('reset_password_sent/', 
+        #auth_views.PasswordResetDoneView.as_view(template_name="cse442-spring2022-team-the-howdies/ratemyclass/main/templates/password_reset_sent.html"), 
+        #name="password_reset_done"),
 
-    path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), 
-     name="password_reset_confirm"),
+    #path('reset/<uidb64>/<token>/',
+     #auth_views.PasswordResetConfirmView.as_view(template_name="cse442-spring2022-team-the-howdies/ratemyclass/main/templates/password_reset_form.html"), 
+     #name="password_reset_confirm"),
 
-    path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), 
-        name="password_reset_complete"),
+    #path('reset_password_complete/', 
+        #auth_views.PasswordResetCompleteView.as_view(template_name="cse442-spring2022-team-the-howdies/ratemyclass/main/templates/password_reset_done.html"), 
+        #name="password_reset_complete"),
 ]
 
 '''
