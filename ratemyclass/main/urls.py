ef39968ce5b9c7c126cf619a444673d76b88371d
Joshua A Donner <jadonner@cheshire.cse.buffalo.edu>
2022-03-18 15:07:53 -0400

    #15 Added paths to urls.py and template of base html pages

--- ratemyclass/main/urls.py
+++ ratemyclass/main/urls.py
@@ -7,5 +7,29 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path('', include("django.contrib.auth.urls")),
     path("register/", reg.register, name="register"),
-    path("password-reset/", ResetPasswordView.as_view(), name="password_reset")
+    #path("password-reset/", ResetPasswordView.as_view(), name="password_reset"),
+
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="main/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="main/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="main/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="main/password_reset_done.html"), 
+        name="password_reset_complete"),
 ]
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
