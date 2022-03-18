3ba7523a077ec2323bd9c6a0fe8abb6c0105fc85
brstrozy <brstrozy@cheshire.cse.buffalo.edu>
2022-03-18 12:58:41 -0400

    13 fixed login bug

--- ratemyclass/main/urls.py
+++ ratemyclass/main/urls.py
@@ -4,7 +4,7 @@ from register import views as reg
 from register.views import ResetPasswordView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('', include("django.contrib.auth.urls")),
     path("register/", reg.register, name="register"),
     path("password-reset/", ResetPasswordView.as_view(), name="password_reset")
