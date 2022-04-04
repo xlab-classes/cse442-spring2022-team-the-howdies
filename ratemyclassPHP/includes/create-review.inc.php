98ce8f4b1399d797958a680993f11a5e1886ae47
nmtry <88214223+nmtry@users.noreply.github.com>
2022-04-04 19:36:32 -0400

    #64 added basic create review backend

--- ratemyclassPHP/includes/create-review.inc.php
+++ ratemyclassPHP/includes/create-review.inc.php
@@ -1,2 +1,23 @@
 <?php 
+if(isset($_POST["submit"])){
+    $title = $_POST["title"];
+    $professor = $_POST["professor"];
+    $review = $_POST["review"];
+    $rating = $_POST["rating"];
+    $classId = $_POST["classId"];
+    $className = $_POST["className"];
+    $ownerId = $_POST["ownerId"];
 
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if(invalidClassId($conn, $classId, $className) !== false){
+        header("location: ../create-review.php?className=". $className . "&classId=" . $classId . "&error=invalid");
+        exit();
+    }
+
+    createReview($conn, $title, $professor, $review, $rating, $classId, $ownerId, $className);
+}else{
+    header("location: ../index.php");
+    exit();
+}
