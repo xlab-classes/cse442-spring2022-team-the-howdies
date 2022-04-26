e8632052fcd845c0af99ec00ef497d0d194dbca3
David Schykerynec <dlschyke@buffalo.edu>
2022-04-25 21:17:53 -0400

    #94 added view-post.php, is identical to view-reviews for now

--- ratemyclassPHP/includes/comments.inc.php
+++ ratemyclassPHP/includes/comments.inc.php
@@ -3,7 +3,12 @@
 
 
 if(isset($_POST["submit"])){
-    
+    $userId = $_POST["professor"];
+    $review = $_POST["review"];
+    $rating = $_POST["rating"];
+    $classId = $_POST["classId"];
+    $className = $_POST["className"];
+    $ownerId = $_POST["ownerId"];
 
     require_once 'dbh.inc.php';
     //require_once 'functions.inc.php';
