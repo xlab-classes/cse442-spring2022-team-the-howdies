e26e7ff3db6ca8526cdd895a0b11d46e162b712a
David Schykerynec <dlschyke@buffalo.edu>
2022-04-25 21:59:24 -0400

    #94 create comment functionality implemented, untested, waiting on front-end implementation

--- ratemyclassPHP/includes/comments.inc.php
+++ ratemyclassPHP/includes/comments.inc.php
@@ -1,25 +1,78 @@
 <?php
 
+function invalidReviewId($conn, $reviewId) {
+    $sql = "SELECT * FROM reviews WHERE reviewsId = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../view-post.php?reviewId=". $reviewId . "&error=invalid");
+        exit();
+    }
 
+    mysqli_stmt_bind_param($stmt, "s", $reviewId);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if($row = mysqli_fetch_assoc($resultData)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function createComment($conn, $userId, $reviewId, $comment) {
+    $sql = "INSERT INTO comments (userId, reviewId, comment) VALUES (?, ?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../view-post.php?reviewId=". $reviewId . "&error=invalid");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "sss", $userId, $reviewId, $comment);
+    mysqli_stmt_execute($stmt);
+
+    $sql = "SELECT * FROM reviews WHERE reviewId = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        header("location: ../view-post.php?reviewId=". $reviewId . "&error=invalid");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt, "s", $reviewId);
+    mysqli_stmt_execute($stmt);
+    $resultData = mysqli_stmt_get_result($stmt);
+    if($row = mysqli_fetch_assoc($resultData)){
+        $total = $row["reviewsTotalComments"];
+
+        $total = $total + 1;
+
+        $sql = "UPDATE reviews SET reviewsTotalComments=? WHERE classesId=?;";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("location: ../view-post.php?reviewId=". $reviewId . "&error=invalid");
+            exit();
+        }
+        mysqli_stmt_bind_param($stmt, "ss", $total, $reviewId);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }else{
+        header("location: ../view-post.php?reviewId=". $reviewId . "&error=invalid");
+        exit();
+    }
+}
 
 if(isset($_POST["submit"])){
-    $userId = $_POST["professor"];
-    $review = $_POST["review"];
-    $rating = $_POST["rating"];
-    $classId = $_POST["classId"];
-    $className = $_POST["className"];
-    $ownerId = $_POST["ownerId"];
+    $userId = $_POST["userId"];
+    $reviewId = $_POST["reviewId"];
+    $comment = $_POST["comment"];
 
     require_once 'dbh.inc.php';
     //require_once 'functions.inc.php';
 
-    if(){
-        header();
+    if(invalidReviewId($conn, $reviewId) == false){
+        header("location: ../view-post.php?reviewId=". $reviewId . "&error=invalid");
         exit();
     }
-
-
 }else{
-    header();
+    header("location: ../view-post.php?reviewId=". $reviewId);
     exit();
 }
