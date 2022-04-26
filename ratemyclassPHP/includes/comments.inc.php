1fcca9f07fd3166c4e86fb1649ebd2e8c8280f0c
David Schykerynec <dlschyke@buffalo.edu>
2022-04-25 20:34:01 -0400

    removed unnecessary code

--- ratemyclassPHP/includes/comments.inc.php
+++ ratemyclassPHP/includes/comments.inc.php
@@ -3,19 +3,18 @@
 
 
 if(isset($_POST["submit"])){
-    $username = $_POST["uid"];
-    $pwd = $_POST["pwd"];
+    
 
     require_once 'dbh.inc.php';
     //require_once 'functions.inc.php';
 
-    if(emptyInputLogin($username, $pwd) !== false){
-        header("location: ../login.php?error=emptyInput");
+    if(){
+        header();
         exit();
     }
 
-    loginUser($conn, $username, $pwd);
+
 }else{
-    header("location: ../login.php");
+    header();
     exit();
 }
